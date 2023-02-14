c6ae4db3ac00144fa44e06c0b504e9f4c5490cd3
tristan <77806722+trstngttschlk@users.noreply.github.com>
2023-02-14 11:40:49 -0500

    add basic layout, project carousel

---
+++ index.js
@@ -0,0 +1,80 @@
+// local projects data
+const projects = [
+   {
+      id: 1,
+      name: "Project 1",
+      img: "./assets/dummy-200x200.png",
+      tech: "Javascript, React, HTML, CSS",
+      liveLink: "http://google.com",
+      codeLink: "https://twitter.com"
+   },
+   {
+      id: 2,
+      name: "Project 2",
+      img: "#",
+      tech: "Javascript, HTML, CSS",
+      liveLink: "#",
+      codeLink: "#"
+   },
+   {
+      id: 3,
+      name: "Project 3",
+      img: "#",
+      tech: "Javascript, HTML, CSS",
+      liveLink: "#",
+      codeLink: "#"
+   },
+   {
+      id: 4,
+      name: "Project 4",
+      img: "#",
+      tech: "Javascript, HTML, CSS",
+      liveLink: "#",
+      codeLink: "#"
+   }
+]
+
+// select carousel projects
+const img = document.getElementById("project-img")
+const name = document.getElementById("project-name")
+const tech = document.getElementById("project-tech")
+const liveLink = document.getElementById("live-link")
+const codeLink = document.getElementById("code-link")
+
+const prevBtn = document.querySelector(".prev-btn")
+const nextBtn = document.querySelector(".next-btn")
+
+// set starting projects
+let currentProject = 0
+
+// load initial project
+window.addEventListener('DOMContentLoaded', () => {
+   showProject(currentProject)
+})
+
+// show project based on project
+function showProject() {
+   const project = projects[currentProject]
+   img.src = project.img
+   name.textContent = project.name
+   tech.textContent = project.tech
+   liveLink.href = project.liveLink
+   codeLink.href = project.codeLink
+}
+
+// show next project
+nextBtn.addEventListener("click", () => {
+   currentProject++
+   if(currentProject > projects.length-1) {
+       currentProject = 0
+   }
+   showProject()
+})
+// show prev project
+prevBtn.addEventListener("click", () => {
+   currentProject--
+   if(currentProject < 0) {
+      currentProject = projects.length-1
+   }
+   showProject()
+})
